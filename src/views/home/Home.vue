f082131845fe489eea34f8a6055c731f035c8610
2356169027@qq.com <2356169027@qq.com>
2021-03-08 16:28:25 +0800

    首页

---
+++ src/views/home/Home.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <home-swiper :banners="banners" />
+    <recommend-view :recommends="recommends" />
+    <feature-view />
+
+    <tab-control class="tab-control" :titles="['流行', '新款', '精选']" />
+    <ul>
+      <li v-for="item in 100">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
+
+import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabControl/TabControl";
+
+import { getHomeMultidata, getHomeGoods } from "network/home";
+export default {
+  name: "Home",
+  components: {
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+
+    NavBar,
+    TabControl
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+      goods: {
+        pop: {
+          page: 0,
+          list: []
+        },
+        news: {
+          page: 0,
+          list: []
+        },
+        sell: {
+          page: 0,
+          list: []
+        }
+      }
+    };
+  },
+  methods: {},
+  created() {
+    getHomeMultidata().then(res => {
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
+    getHomeGoods("pop", 1).then(res => {
+      console.log(res);
+    });
+  }
+};
+</script>
+
+<style scoped>
+#home {
+  padding-top: 44px;
+}
+.home-nav {
+  color: #fff;
+  background-color: var(--color-tint);
+
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  z-index: 1;
+}
+.tab-control {
+  position: sticky;
+  top: 44px;
+}
+</style>
